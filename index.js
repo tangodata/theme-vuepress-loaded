d34d5a63c06142594747abe03e2dfa1c29918464
Kace at Telematique <kaceong@gmail.com>
2019-06-21 17:39:25 +0800

    remove sitemap

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ extend: '@vuepress/theme-default',
 ////////////////////////////////////
 
 head: [
+  ['meta', {
+    name: "generator",
+    content: "Vuepress and Bulma"
+  }],
   ['link', {
-    rel: "icon",
+    rel: "shortcut icon",
     href: "/favicon.ico"
   }],
 
@@ -82,49 +86,51 @@ head: [
 ////////////////////////////////////
 
 plugins: [
-    'clean-urls',
-    'named-chunks',
-    '@vuepress/medium-zoom',
-    '@vuepress/back-to-top',
-    '@vuepress/nprogress',
-    ['dehydrate',{
-      //noSSR: '404.html',
-      //noScript: '**/*.html'
-      //without script, does slide menu work?
-    }],
-    ['@vuepress/pwa', {
-      serviceWorker: true,
-      updatePopup: {
-        message: "New content is available.",
-        buttonText: "Refresh"
-      }
-    }],
-    'seo',
-    'sitemap',
+  'seo',
+  'clean-urls',
+  'named-chunks',
+  '@vuepress/medium-zoom',
+  '@vuepress/back-to-top',
+  '@vuepress/nprogress',
+  ['dehydrate',{
+    //noSSR: '404.html',
+    //noScript: '**/*.html'
+    //without script, does slide menu work?
+  }],
+  ['@vuepress/pwa', {
+    serviceWorker: true,
+    updatePopup: {
+      message: "New content is available.",
+      buttonText: "Refresh"
+    }
+  }],
 
   /*:
-    'vuepress-plugin-blog-multidir',
-    'blog-multidir',
-    'alias',
-    'tabs',
+  ['sitemap',{'hostname':'https://'}],
 
-    ['@vuepress/blog', {
-      postsDir: 'posts',
-      permalink: '/:year/:month/:slug'
-    }],
+  'vuepress-plugin-blog-multidir',
+  'blog-multidir',
+  'alias',
+  'tabs',
 
-    ['container', {
-      type: 'right',
-      defaultTitle: '',
-    }],
+  ['@vuepress/blog', {
+    postsDir: 'posts',
+    permalink: '/:year/:month/:slug'
+  }],
 
-    ['container', {
-      type: 'theorem',
-      before: info => `<div class="theorem"><p class="title">${info}</p>`,
-      after: '</div>',
-    }],
-  */
-  ],
+  ['container', {
+    type: 'right',
+    defaultTitle: '',
+  }],
+
+  ['container', {
+    type: 'theorem',
+    before: info => `<div class="theorem"><p class="title">${info}</p>`,
+    after: '</div>',
+  }],
+*/
+
+],
 
 ////////////////////////////////////
 // Markdown - add several syntax
@@ -132,27 +138,27 @@ plugins: [
 // attribute class
 ////////////////////////////////////
 
-  markdown: {
-    toc: { includeLevel: [1, 2] },
-    anchor: { permalink: false },
-    extendMarkdown: md => {
-      // use more markdown-it plugins!
-      md.use(require('markdown-it-deflist'))
-      md.use(require('markdown-it-attrs'))
+markdown: {
+  toc: { includeLevel: [1, 2] },
+  anchor: { permalink: false },
+  extendMarkdown: md => {
+    // use more markdown-it plugins!
+    md.use(require('markdown-it-deflist'))
+    md.use(require('markdown-it-attrs'))
 /*
-      md.use(require('markdown-it-wikicustom')
-        ({
-          baseURL: '/wiki/',
-          makeAllLinksAbsolute: true,
-          htmlAttributes: {
-            'class':'wikilink',
-            'rel':'nofollow'
-          }
-        })
-      )
+    md.use(require('markdown-it-wikicustom')
+      ({
+        baseURL: '/wiki/',
+        makeAllLinksAbsolute: true,
+        htmlAttributes: {
+          'class':'wikilink',
+          'rel':'nofollow'
+        }
+      })
+    )
 */
-    },
   },
+},
 
 ////////////////////////////////////
 // Webpack modifications
