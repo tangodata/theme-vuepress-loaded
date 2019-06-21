dbec3912ff8f61fd920c8826fdc17fa720d6c1c7
Kace at Telematique <kaceong@gmail.com>
2019-06-21 17:07:51 +0800

    move favicon

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-//const path = require('path')
-
-module.exports = (options, ctx) => ({
-  extend: '@vuepress/theme-default',
+module.exports =
+(options, ctx) => ({
+extend: '@vuepress/theme-default',
 
 ////////////////////////////////////
 // Stylesheet includes
@@ -9,66 +8,68 @@ module.exports = (options, ctx) => ({
 // fontawesome
 ////////////////////////////////////
 
-  head: [
-    ['link', {
-      rel: "icon",
-      href: "/favicon.ico"
-    }],
+head: [
+  ['link', {
+    rel: "icon",
+    href: "/favicon.ico"
+  }],
+
 /*
-    ['link', {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      href: "/assets/favicons/apple-touch-icon.png"
-    }],
-    ['link', {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      href: "/assets/favicons/favicon-32x32.png"
-    }],
-    ['link', {
-      rel: "icon",
-      type: "image/png",
-      sizes: "16x16",
-      href: "/assets/favicons/favicon-16x16.png"
-    }],
-    ['link', {
-      rel: "manifest",
-      href: "/assets/favicons/site.webmanifest"
-    }],
-    ['link', {
-      rel: "mask-icon",
-      href: "/assets/favicons/safari-pinned-tab.svg",
-      color: "#3a0839"
-    }],
-    ['link', {
-      rel: "shortcut icon",
-      href: "/assets/favicons/favicon.ico"
-    }],
-    ['meta', {
-      name: "msapplication-TileColor",
-      content: "#3a0839"
-    }],
-    ['meta', {
-      name: "msapplication-config",
-      content: "/assets/favicons/browserconfig.xml"
-    }],
-    ['meta', {
-      name: "theme-color",
-      content: "#ffffff"
-    }],
-    ['link', {
-      rel: 'stylesheet',
-      href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'
-    }],
-    ['link', {
-      rel: 'stylesheet',
-      href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css',
-      integrity: 'sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ',
-      crossorigin: 'anonymous'
-    }]
-  ],
+  ['link', {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/assets/favicons/apple-touch-icon.png"
+  }],
+  ['link', {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/assets/favicons/favicon-32x32.png"
+  }],
+  ['link', {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/assets/favicons/favicon-16x16.png"
+  }],
+  ['link', {
+    rel: "manifest",
+    href: "/assets/favicons/site.webmanifest"
+  }],
+  ['link', {
+    rel: "mask-icon",
+    href: "/assets/favicons/safari-pinned-tab.svg",
+    color: "#3a0839"
+  }],
+  ['link', {
+    rel: "shortcut icon",
+    href: "/assets/favicons/favicon.ico"
+  }],
+  ['meta', {
+    name: "msapplication-TileColor",
+    content: "#3a0839"
+  }],
+  ['meta', {
+    name: "msapplication-config",
+    content: "/assets/favicons/browserconfig.xml"
+  }],
+  ['meta', {
+    name: "theme-color",
+    content: "#ffffff"
+  }],
+  ['link', {
+    rel: 'stylesheet',
+    href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'
+  }],
+  ['link', {
+    rel: 'stylesheet',
+    href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css',
+    integrity: 'sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ',
+    crossorigin: 'anonymous'
+  }]
 */
+],
+
 
 ////////////////////////////////////
 // Vuepress Plugins -- add features
