d058ee398ae2a3ebfae16f651aedaee43c4af70c
Kace at Telematique <kaceong@gmail.com>
2019-06-21 20:04:19 +0800

    remove ads

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -4,19 +4,22 @@ ParentLayout
 
 <script>
 import ParentLayout from '@parent-theme/layouts/Layout.vue'
-//import CarbonAds    from '@theme/components/CarbonAds.vue'
-//import BuySellAds   from '@theme/components/BuySellAds.vue'
 
 export default {
   components: {
     ParentLayout,
-//    CarbonAds,
-//    BuySellAds
   }
 }
 /*
 ParentLayout
   CarbonAds(slot="sidebar-top")
   BuySellAds(slot="page-bottom")
+
+//    CarbonAds,
+//    BuySellAds
+
+//Test2 import CarbonAds    from '@theme/components/CarbonAds.vue'
+//Test1 import BuySellAds   from '@theme/components/BuySellAds.vue'
+
 */
 </script>
